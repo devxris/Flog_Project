d5b834f9f7b1de3bada2011ef82e3b85943533a7
devxris <chuwei0915@gmail.com>
2019-05-23 16:43:10 +0800

    Update Profile

--- Project/users/models.py
+++ Project/users/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 # manual import
 from django.contrib.auth.models import User
+from PIL import Image
 
 
 # Create your models here.
@@ -12,3 +13,12 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.user.username} Profile"
+
+    # override built-in function and resize upload image
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        img = Image.open(self.image.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
